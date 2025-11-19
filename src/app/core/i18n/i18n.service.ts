0187afe084fefb7f8cf48f42b7d44951f7ecd22c
kevin <1622929837@qq.com>
2025-11-19 16:59:47 +0800

    回档至angular19状态，此后更新以此为基准

--- src/app/core/i18n/i18n.service.ts
+++ src/app/core/i18n/i18n.service.ts
@@ -71,8 +71,8 @@ export class I18NService extends AlainI18nBaseService {
     return { code, text: item.text, abbr: item.abbr };
   });
 
-  constructor() {
-    super();
+  constructor(private cov: AlainConfigService) {
+    super(cov);
 
     const defaultLang = this.getDefaultLang();
     this._defaultLang = this._langs.findIndex(w => w.code === defaultLang) === -1 ? DEFAULT : defaultLang;
