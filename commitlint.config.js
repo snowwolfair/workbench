ae1239a6e3010ed530e4d62313a307486d0fd330
kevin <1622929837@qq.com>
2025-11-26 16:14:44 +0800

    fix(commitlint): 修改配置文件

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     "type-enum": [2, 'always', [
-      'feat', 'fix', 'docs', 'perf', 'revert', 'ci', 'test', 'refactor', 'build', 'style', 'chore'
+      'feat', 'fix', 'docs', 'perf', 'revert', 'ci', 'test', 'refactor', 'build', 'style', 'chore', 'merge'
     ]],
     'type-case': [0],//type 的输入格式,默认为小写‘lower-case’
     'type-empty': [0],//type 是否可为空
